13fb351e39eb5406b78c389c0cfc4b810dd1acab
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2018-10-28 13:43:01 +0100

    Add new locales

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ set :url_root, ENV.fetch('BASE_URL')
 
 ignore '/templates/*'
 
-activate :i18n, langs: [:en, :it], mount_at_root: false
+activate :i18n, langs: [:en, :it, :es, :fr, :zh], mount_at_root: false
 activate :asset_hash
 activate :directory_indexes
 activate :pagination
