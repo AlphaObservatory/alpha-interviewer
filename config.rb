87d897890fadc7fc555c1e79b0aa26383a7553bf
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2023-06-27 23:08:16 +0200

    Create steps ni turkish

--- config.rb
+++ config.rb
@@ -66,7 +66,7 @@ end
 proxy "/_redirects", "/templates/redirects.txt"
 
 dato.tap do |dato|
-  [:en, :it, :es, :fr, :pt, :zh].each do |locale|
+  [:en, :it, :es, :fr, :pt, :zh, :tr].each do |locale|
     I18n.with_locale(locale) do
       dato.steps.each do |step|
         I18n.locale = locale
